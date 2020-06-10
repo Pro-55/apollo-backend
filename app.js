a9b5d6cec110a7fe7db0897b3a5d2383fe6a9650
Pranit Rane <rane.pranit.b@gmail.com>
2020-06-10 16:05:03 +0530

    update: 1. cors 2. http headers with body parser

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const Express = require('express')
+const http = require('http')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const connectDB = require('./connections')
 const { ApolloServer } = require('apollo-server-express')
 const typeDefs = require('./schema')
@@ -8,6 +11,23 @@ require('dotenv/config')
 const port = process.env.PORT
 
 const app = Express()
+
+const corsOptions = {
+  credentials: true
+}
+
+app.use(
+  cors(corsOptions),
+  bodyParser.json({ limit: '50mb' }),
+  bodyParser.urlencoded({ extended: true })
+)
+
+app.use((req, res, next) => {
+  res.removeHeader("X-Powered-By")
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 const server = new ApolloServer({
   typeDefs,
@@ -23,9 +43,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to PASS API')
 })
 
-server.applyMiddleware({ app })
+server.applyMiddleware({ app, path: '/graphql' })
 
-app.listen({ port }, () => callBack())
+const httpServer = http.createServer(app)
+server.installSubscriptionHandlers(httpServer)
+
+httpServer.listen({ port }, () => callBack());
 
 function callBack() {
   console.log('Server is up on: http://localhost:' + port + server.graphqlPath)
